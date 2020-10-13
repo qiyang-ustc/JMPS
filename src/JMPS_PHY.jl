cd6656cb5ecaf69029e4c557c2450a41077b1c6c
Qi Yang <qiyang@mail.ustc.edu.cn>
2020-10-13 16:35:15 +0800

    singular spectrum

--- src/JMPS_PHY.jl
+++ src/JMPS_PHY.jl
@@ -94,3 +94,45 @@ function entropy(mps::MPS)
     
     return v_entropy
 end
+
+function entropy(mps::MPS,bond_id::Int)
+    # Calculate the von-Neumann entanglement entropy of an MPS
+    #from left to right, svd 
+    res = 0.0
+    @assert bond_id>0 && bond_id <= mps.L
+    for site =1:bond_id-1
+        l=mps.bdim[site-1] # left bond dimension
+        r=mps.bdim[site]   # current bond dimension
+        A=reshape(mps[site],(l*mps.S,r)) # A is a matrix unfolded from the current tensor
+        U, R = qr(A) # here we intent to do QR = A. However there is no BP, so we do SVD instead 
+        U = Array(U)
+        s = norm(R)
+        res = res + log(s)
+        R = R./s # devided by norm
+        Dnew = size(R)[1]
+        mps[site] = reshape(U[:,1:Dnew],l,mps.S,Dnew)   # U is LinearAlgebra.QRCompactWYQ type,it is not normal array
+        mps[site+1] = reshape(R*reshape(mps[site+1],r,:),:,mps.S,mps.bdim[site+1])
+        mps.bdim[site] = Dnew
+    end
+    for site = mps.L:-1:bond_id+2
+        l = mps.bdim[site-1]
+        r = mps.bdim[site]
+        A = reshape(mps[site],(l, r*mps.S))
+        A = transpose(A)
+        U,R = qr(A) # UR = A^{T} R^{T}U^{T} = A
+        Dnew = size(R)[1]
+        U = transpose(Array(U))
+        R = transpose(R)
+        mps[site] = reshape(U,(Dnew,mps.S,r))
+        mps[site-1] = reshape(reshape(mps[site-1],mps.bdim[site-2]*mps.S,mps.bdim[site-1])* R ,(mps.bdim[site-2], mps.S, Dnew))
+        mps.bdim[site-1] = Dnew
+        #print(mps.bdim)
+        #print ('addition:', res)
+    end
+
+    l = reshape(mps[bond_id],mps.bdim[bond_id-1]*mps.S,mps.bdim[bond_id])
+    r = reshape(mps[bond_id+1],mps.bdim[bond_id],mps.S*mps.bdim[bond_id+1])
+    U,S,V = svd(l*r)
+
+    return S
+end
