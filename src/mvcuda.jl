3463ee7400951eddb158023ee55e15e048dc5fb9
Qi Yang <qiyang@mail.ustc.edu.cn>
2020-11-07 00:38:39 +0800

    cu for mps and mpo

--- src/mvcuda.jl
+++ src/mvcuda.jl
@@ -5,6 +5,6 @@ function cu(mpo::MPO{FloatType,Array{FloatType,4}}) where FloatType<:Number
     return MPO{FloatType,CuArray{FloatType,4}}(mpo.L,mpo.S,mpo.bdim,map(CuArray,mpo.tensor))
 end
 
-function cu(mpo::MPS{FloatType,Array{FloatType,3}}) where FloatType<:Number
-    return MPS{FloatType,CuArray{FloatType,3}}(mpo.L,mpo.S,mpo.bdim,map(CuArray,mpo.tensor))
+function cu(mps::MPS{FloatType,Array{FloatType,3}}) where FloatType<:Number
+    return MPS{FloatType,CuArray{FloatType,3}}(mps.L,mps.S,mps.bdim,map(CuArray,mps.tensor))
 end
