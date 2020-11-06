0d1f91d18f909d52e0608d1c5e3aa00e71e00bf0
Qi Yang <qiyang@mail.ustc.edu.cn>
2020-11-07 00:38:30 +0800

    cu for mps and mpo

--- src/mvcuda.jl
+++ src/mvcuda.jl
@@ -1,3 +1,10 @@
-function cuda(mpo::MPO{FloatType,Array{FloatType,4}} where FloatType<:Number)
+import CUDA.cu
+export cu
+
+function cu(mpo::MPO{FloatType,Array{FloatType,4}}) where FloatType<:Number
     return MPO{FloatType,CuArray{FloatType,4}}(mpo.L,mpo.S,mpo.bdim,map(CuArray,mpo.tensor))
+end
+
+function cu(mpo::MPS{FloatType,Array{FloatType,3}}) where FloatType<:Number
+    return MPS{FloatType,CuArray{FloatType,3}}(mpo.L,mpo.S,mpo.bdim,map(CuArray,mpo.tensor))
 end
