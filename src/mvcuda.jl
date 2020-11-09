445b09ec425e0520556155245104df5ef2098827
qiyang-ustc <qiyang@mail.ustc.edu.cn>
2020-11-09 04:25:36 +0000

    export array : mv cuda array to CPU

--- src/mvcuda.jl
+++ src/mvcuda.jl
@@ -1,5 +1,5 @@
 import CUDA.cu
-export cu
+export cu,array
 
 function cu(mpo::MPO{FloatType,Array{FloatType,4}}) where FloatType<:Number
     return MPO{FloatType,CuArray{FloatType,4}}(mpo.L,mpo.S,mpo.bdim,map(CuArray,mpo.tensor))
