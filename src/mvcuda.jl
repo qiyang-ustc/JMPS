1894cdfb4993d9d82d9ec872e3c2a00a10c2682c
qiyang-ustc <qiyang@mail.ustc.edu.cn>
2020-11-09 04:08:29 +0000

    extend mv cuda to mv cuda and mv to cpu:

--- src/mvcuda.jl
+++ src/mvcuda.jl
@@ -1,3 +1,13 @@
 function cuda(mpo::MPO{FloatType,Array{FloatType,4}} where FloatType<:Number)
     return MPO{FloatType,CuArray{FloatType,4}}(mpo.L,mpo.S,mpo.bdim,map(CuArray,mpo.tensor))
 end
+function cuda(mps::MPO{FloatType,Array{FloatType,3}} where FloatType<:Number)
+    return MPS{FloatType,CuArray{FloatType,3}}(mps.L,mps.S,mps.bdim,map(CuArray,mps.tensor))
+end
+
+function array(mpo::MPO{FloatType,CuArray{FloatType,4}} where FloatType<:Number)
+    return MPO{FloatType,Array{FloatType,4}}(mpo.L,mpo.S,mpo.bdim,map(Array,mpo.tensor))
+end
+function array(mps::MPO{FloatType,CuArray{FloatType,3}} where FloatType<:Number)
+    return MPS{FloatType,Array{FloatType,3}}(mps.L,mps.S,mps.bdim,map(Array,mps.tensor))
+end
